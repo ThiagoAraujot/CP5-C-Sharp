893608a02220c0a18ad0f9d474191dbe7d568afb
victor-otsuga <victorsantosp10@hotmail.com>
2026-04-27 20:25:43 -0300

    feat: add ASP.NET Core MVC project setup with MySQL and BCrypt

---
+++ GameStoreMVC/Program.cs
@@ -0,0 +1,67 @@
+using GameStoreMVC.Data;
+using GameStoreMVC.Interfaces;
+using GameStoreMVC.Models;
+using GameStoreMVC.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/Login";
+        options.ExpireTimeSpan = TimeSpan.FromHours(8);
+        options.SlidingExpiration = true;
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+
+    if (!db.Usuarios.Any(u => u.Email == "admin@gamestore.com"))
+    {
+        db.Usuarios.Add(new Usuario
+        {
+            Nome = "Admin",
+            Email = "admin@gamestore.com",
+            SenhaHash = BCrypt.Net.BCrypt.HashPassword("Admin@123"),
+            Role = "Admin"
+        });
+        db.SaveChanges();
+    }
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
